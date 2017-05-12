90554a8eb10f09125700136351c095f75c9093af
farvher <fesanmiguel@misena.edu.co>
2017-05-12 16:46:36 -0500

    first commit

---
+++ src/main/java/com/eltiempo/ceetchat/CeetchatApplication.java
@@ -0,0 +1,12 @@
+package com.eltiempo.ceetchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeetchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeetchatApplication.class, args);
+	}
+}
